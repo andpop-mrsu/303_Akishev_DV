ad825dd39b9b3aa35c3b90bbd7f2fbe3fde4616a
daniil_A <RLegar@mail.ru>
2021-11-16 23:19:26 +0300

    Task05

--- Task05/data_add.sql
+++ Task05/data_add.sql
@@ -10,23 +10,9 @@ INSERT INTO users(name, surname, email, gender, register_date, occupation_id) VA
 ("Irina", "Guskova", "irina_guskova@mail.ru", "female", DATE("now"), (SELECT id FROM occupations WHERE occupations.title = "student"));
 
 
-INSERT INTO movies(title, year) VALUES
-("Human anger", 2021);
-INSERT INTO movies(title, year) VALUES
-("Dune", 2021);
-INSERT INTO movies(title, year) VALUES
-("Venom 2", 2021);
-
-
-INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES(
-(SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), 
-(SELECT id FROM movies WHERE movies.title = "Human anger" and movies.year = 2021),
-4, strftime('%s','now'));
-INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES(
-(SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), 
-(SELECT id FROM movies WHERE movies.title = "Dune" and movies.year = 2021),
-4.5, strftime('%s','now'));
-INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES(
-(SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), 
-(SELECT id FROM movies WHERE movies.title = "Venom 2" and movies.year = 2021),
-5, strftime('%s','now'));
+INSERT INTO movies(title, year) VALUES("Human anger", 2021);
+INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES((SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), (SELECT id FROM movies WHERE movies.title = "Human anger" and movies.year = 2021), 4, strftime('%s','now'));
+INSERT INTO movies(title, year) VALUES("Dune", 2021);
+INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES((SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), (SELECT id FROM movies WHERE movies.title = "Dune" and movies.year = 2021), 4.5, strftime('%s','now'));
+INSERT INTO movies(title, year) VALUES("Venom 2", 2021);
+INSERT INTO ratings(user_id, movie_id, rating, "timestamp") VALUES((SELECT id FROM users WHERE users.email = "daniil_akishev@mail.ru"), (SELECT id FROM movies WHERE movies.title = "Venom 2" and movies.year = 2021), 5, strftime('%s','now'));
